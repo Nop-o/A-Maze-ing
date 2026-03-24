2318b27aebd885da250859ab448a47b42305538a
Amine Slimani <aslimani@z4r10p4.42lyon.fr>
2026-03-24 15:27:28 +0100

    add solver algo

--- grid.py
+++ grid.py
@@ -1,11 +1,11 @@
 class Grid:
     NORTH = 0b0001
-    EAST  = 0b0010
+    EAST = 0b0010
     SOUTH = 0b0100
-    WEST  = 0b1000
+    WEST = 0b1000
 
     OPPOSITE = {NORTH: SOUTH, SOUTH: NORTH, EAST: WEST, WEST: EAST}
-    DELTA = {NORTH: (0,-1), SOUTH: (0,1), EAST: (1,0), WEST: (-1,0)}
+    DELTA = {NORTH: (0, -1), SOUTH: (0, 1), EAST: (1, 0), WEST: (-1, 0)}
 
     def __init__(self, width: int, height: int) -> None:
         self.width = width
@@ -15,34 +15,33 @@ class Grid:
 
     def create_grid(self) -> None:
         self.cells = []
-        for y in range(self.height): # for each line
+        for y in range(self.height):  # for each line
             line: list[int] = []
-            for x in range(self.width): # for each column
+            for x in range(self.width):  # for each column
                 line.append(0xF)
-            self.cells.append(line) # grid[y][x]
-
+            self.cells.append(line)  # grid[y][x]
 
     def remove_wall(self, x: int, y: int, direction: int) -> None:
-        
-        dx, dy = self.DELTA[direction] # direction to go towards the neighboring cell
+
+        dx, dy = self.DELTA[direction]  # direction to go towards the neighboring cell
         nx = x + dx
         ny = y + dy
-        
+
         if not self.is_valid(nx, ny):
             return
 
-        self.cells[y][x] &= ~direction # breaks down the cell wall
-        self.cells[ny][nx] &= ~self.OPPOSITE[direction] # breaks down the wall of the neighboring cell
+        self.cells[y][x] &= ~direction  # breaks down the cell wall
+        self.cells[ny][nx] &= ~self.OPPOSITE[direction]  # breaks down the wall of the neighboring cell
 
     def is_valid(self, x: int, y: int) -> bool:
         if 0 <= x < self.width and 0 <= y < self.height:
             return True
         return False
-    
-    def display(self) -> None: # ffunction that tests the grid by displaying in ASCII
-        print("+" + "---+" * self.width) # top line
 
-        for y in range(self.height): # middle line
+    def display(self) -> None:  # ffunction that tests the grid by displaying in ASCII
+        print("+" + "---+" * self.width)  # top line
+
+        for y in range(self.height):  # middle line
             row = "|"
             for x in range(self.width):
                 row += "   "
@@ -50,17 +49,18 @@ class Grid:
             print(row)
 
             bot = "+"
-            for x in range(self.width): # bot line
+            for x in range(self.width):  # bot line
                 bot += "---+" if self.cells[y][x] & self.SOUTH else "   +"
             print(bot)
 
 
-if __name__=="__main__":  # main test
+if __name__ == "__main__":  # main test
     g = Grid(5, 5)
     print(g.cells[0][0])
     print(g.cells[0][1])
 
     g.remove_wall(0, 0, Grid.EAST)
+    g.remove_wall(0, 1, Grid.SOUTH)
 
     print(g.cells[0][0])
     print(g.cells[0][1])
--- maze_generator.py
+++ maze_generator.py
@@ -1,9 +1,12 @@
 import random
 from grid import Grid
+from collections import deque
+
 
 class MazeGenerator:
     def __init__(self, width: int, height: int, entry: tuple[int, int],
-                 exit: tuple[int, int], perfect: bool, seed: int | None) -> None:
+                 exit: tuple[int, int],
+                 perfect: bool, seed: int | None) -> None:
         self.width = width
         self.height = height
         self.entry = entry
@@ -32,7 +35,8 @@ class MazeGenerator:
             else:
                 stack.pop()
 
-    def get_unvisited_neighbors(self, cell: tuple[int, int], visited: set) -> list:
+    def get_unvisited_neighbors(self, cell: tuple[int, int],
+                                visited: set) -> list:
         g = self.grid
         x, y = cell
         neighbors = []
@@ -44,8 +48,47 @@ class MazeGenerator:
                 neighbors.append(direction)
         return neighbors
 
+    def solver_bfs(self):
+        parent = {self.entry: None}
+        visited = {self.entry}
+        queue = deque([self.entry])
+
+        while queue:
+            print("1")
+            current_cell = queue.popleft()
+            if current_cell == self.exit:
+                return parent
+            print("2")
+            x, y = current_cell
+            for neighbor in self.get_neighbors(x, y):
+                print(self.get_neighbors(x, y))
+                print("3")
+                if neighbor not in visited:
+                    print("5")
+                    visited.add(neighbor)
+                    parent[neighbor] = current_cell
+                    queue.append(neighbor)
+                    print("4")
+        return None
+
+    def get_neighbors(self, x: int, y: int) -> list[tuple[int, int]]:
+        g = self.grid
+        neighbors = []
+
+        for direction in [g.NORTH, g.SOUTH, g.EAST, g.WEST]:
+            if not (g.cells[y][x] & direction):
+                dx, dy = g.DELTA[direction]
+                nx, ny = x + dx, y + dy
+                if g.is_valid(nx, ny):
+                    neighbors.append((nx, ny))
+        return neighbors
+
+    def get_path_way(parent: dict) -> dict:
+        pass
+
 
 if __name__ == "__main__":
-    mg = MazeGenerator(10, 10, (0,0), (9,9), perfect=True, seed=42)
+    mg = MazeGenerator(10, 10, (0, 0), (9, 9), perfect=True, seed=None)
     mg.generate_maze_dfs()
     mg.grid.display()
+    print(mg.solver_bfs())
