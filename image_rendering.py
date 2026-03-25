afd7991888d89071d2ad607de727fe10c20efd5f
adamez-f <adamez-f@student.42lyon.fr>
2026-03-25 13:29:44 +0100

    updated_hexa_display and palette

--- image_rendering.py
+++ image_rendering.py
@@ -1,13 +1,13 @@
-palette = {0: ("  ", "  "),
-        1: ("--", "  "),
-        2: (" |", " |"),
-        3: ("-+", " |"),
-        4: ("  ", "--"),
-        5: ("--", "--"),
-        6: (" |", "-+"),
-        7: ("-+", "-+"),
-        8: ("| ", "| "),
-        9: ("+-", "| "),
+palette = {'0': ("  ", "  "),
+        '1': ("--", "  "),
+        '2': (" |", " |"),
+        '3': ("-+", " |"),
+        '4': ("  ", "--"),
+        '5': ("--", "--"),
+        '6': (" |", "-+"),
+        '7': ("-+", "-+"),
+        '8': ("| ", "| "),
+        '9': ("+-", "| "),
         'a': ("||", "||"),
         'b': ("++", "||"),
         'c': ("| ", "+-"),
@@ -15,3 +15,12 @@ palette = {0: ("  ", "  "),
         'e': ("||", "++"),
         'f': ("++", "++"),
 }
+
+def display_maze_from_hexa(hexa_maze: list[str], palette: dict[str, tuple[str]]) -> None:
+    for lign_part_1 in hexa_maze:
+        lign_part_2: list[str] = []
+        for i in range(len(lign_part_1)):
+            print(palette[lign_part_1[i]][0])
+            lign_part_2.append(palette[lign_part_1[i]][0])
+        print()
+        print("".join(lign_part_2))
