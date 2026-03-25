9abce8511c75c3f2433bab0863b9349b929507bf
devamine <asl.amislim@gmail.com>
2026-03-25 16:55:48 +0100

    test

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -4,5 +4,4 @@ from parsing.parsing_file import parse_input_file
 
 def a_maze_ing() -> None:
     parsed_input = parse_input_file("input.txt")
-    if parsed_input:
-        print(parsed_input)
+    MazeGenerator(**parsed_input)
