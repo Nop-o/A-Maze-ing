b5b5593edfa9fb361973e21dcd0a634501d2b06d
devamine <asl.amislim@gmail.com>
2026-03-27 11:04:04 +0100

    fix main file

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,4 +1,4 @@
-from maze_generator import MazeGenerator
+from algo_dfs import DepthFirstSearch
 from parsing.parsing_file import parse_input_file
 import sys
 from image_rendering import MazeDisplay
@@ -9,31 +9,31 @@ def a_maze_ing(file_name: str) -> None:
     if not maze_setting:
         return
     try:
-        maze = MazeGenerator(maze_setting.width,
-                             maze_setting.height,
-                             (maze_setting.entry_x, maze_setting.entry_x),
-                             (maze_setting.exit_x, maze_setting.exit_x),
-                             perfect=maze_setting.is_perfect,
-                             seed=maze_setting.seed)
+        dfs = DepthFirstSearch(maze_setting.width,
+                               maze_setting.height,
+                               (maze_setting.entry_x, maze_setting.entry_x),
+                               (maze_setting.exit_x, maze_setting.exit_x),
+                               perfect=maze_setting.is_perfect,
+                               seed=maze_setting.seed)
     except ValueError as e:
         print(e)
 
-    maze.generate_maze_dfs()
-    hexa_maze = maze.create_hexa_maze()
-    perfect_maze_path = maze.solver_bfs()
+    dfs.generate()
+    hexa_maze = dfs.create_hexa_maze()
+    perfect_maze_path = dfs.solver()
     try:
-        cardinal_path = maze.find_cardinal_path(perfect_maze_path)
+        cardinal_path = dfs.find_cardinal_path(perfect_maze_path)
     except ValueError as e:
         print(e)
     else:
-        maze.print_maze_to_file(maze_setting.output_filename,
-                                hexa_maze, cardinal_path)
-    print(maze.solver_bfs())
+        dfs.print_maze_to_file(maze_setting.output_filename,
+                               hexa_maze, cardinal_path)
+    print(dfs.solver())
 
     MazeDisplay.display_maze_from_hexa2(hexa_maze)
     print("\n\n")
     MazeDisplay.display_maze_from_hexa(hexa_maze)
-    maze.grid.display()
+    dfs.grid.display()
 
 
 if __name__ == "__main__":
