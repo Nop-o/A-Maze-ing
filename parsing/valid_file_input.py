ca3f53756bb864ccd913bde5205b39bbb2d6b755
devamine <asl.amislim@gmail.com>
2026-04-03 17:16:16 +0200

    fix condition file .txt

--- parsing/valid_file_input.py
+++ parsing/valid_file_input.py
@@ -23,7 +23,7 @@ class ValidFileInput(BaseModel):
 
     @model_validator(mode='after')
     def validate_input(self) -> 'ValidFileInput':
-        if not '.' in self.output_filename:
+        if '.' not in self.output_filename:
             raise ValueError("File input is invalid : no '.' detected")
         name, type = self.output_filename.split('.', 1)
         if not name or type != 'txt':
